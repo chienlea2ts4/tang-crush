819ec7414c632e7ff8fab9a58b6e0d688be121c5
chienlea2ts4 <89354478+chienlea2ts4@users.noreply.github.com>
2021-08-23 01:18:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Thế làm người yêu anh nhá:))).',
     btnAccept: 'Okiiiii lun <3',
-    messLink: https://www.facebook.com/chiendeptraizlhihi/
+    
 }
